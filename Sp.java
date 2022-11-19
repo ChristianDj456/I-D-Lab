26782fb04b9868d46f3b21ad6d5900e8ddec73a7
ChristianDj456 <79539982+ChristianDj456@users.noreply.github.com>
2022-11-18 20:03:43 -0500

    Add files via upload

---
+++ Sp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab_f;
+
+public class Sp { //Se define una clase con las coordenadas X y Y.
+    int X;
+    int Y;
+    int Posi;
+
+    public Sp(int Xx, int Yy, int Posis) {
+        this.X = Xx;
+        this.Y = Yy;
+        this.Posi = Posis;
+    }
+}
